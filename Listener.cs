3bf26ee7a274f8d71d3d761ef34da04afb8b1b90
Daniel Barbella <dpbarbella@hotmail.com>
2019-01-21 05:35:09 -0500

    Add project files.

---
+++ Listener.cs
@@ -0,0 +1,109 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System;
+
+namespace Barbella.Utils.ActiveMQ
+{
+    public sealed class Listener
+    {
+        //====================
+        // 
+        //====================
+        public delegate void MapMessageReceived(IMapMessage mapMessage);
+        public event MapMessageReceived OnMapMessageReceived;
+        public delegate void TextMessageReceived(ITextMessage textMessage);
+        public event TextMessageReceived OnTextMessageReceived;
+
+        //====================
+        // 
+        //====================
+        private IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private ISession _session;
+
+        private readonly string _topic;
+
+        public Listener(string uri, string topic)
+        {
+            try
+            {
+                _topic = string.Concat(topic, "?consumer.dispatchAsync=false&consumer.prefetchSize=1&consumer.retroactive=true");
+
+                _connectionFactory = new ConnectionFactory(uri);
+                _connection = _connectionFactory.CreateConnection();
+                _connection.Start();
+
+                _session = _connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                topic = null;
+                uri = null;
+            }
+        }
+
+        public void Start()
+        {
+            IDestination destination;
+            IMessage message;
+            ITextMessage textMessage;
+            IMapMessage mapMessage;
+
+            try
+            {
+                destination = _session.GetTopic(_topic);
+
+                //destination = _session.GetDestination(DESTINATION);
+
+                //====================
+                // 
+                //====================
+                using (IMessageConsumer consumer = _session.CreateConsumer(destination))
+                {
+                    while (true)
+                    {
+                        message = consumer.Receive();
+
+                        if (message != null)
+                        {
+                            mapMessage = message as IMapMessage;
+
+                            if (mapMessage != null)
+                            {
+                                OnMapMessageReceived?.Invoke(mapMessage);
+                            }
+                            else
+                            {
+                                textMessage = message as ITextMessage;
+
+                                if (textMessage != null)
+                                {
+                                    OnTextMessageReceived?.Invoke(textMessage);
+                                }
+                                else
+                                {
+                                    //NEEDS WORK HERE...
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                destination = null;
+                mapMessage = null;
+                message = null;
+                textMessage = null;
+            }
+        }
+    }
+}
---
+++ Producer.cs
@@ -0,0 +1,76 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System;
+using System.Collections.Generic;
+
+namespace Barbella.Utils.ActiveMQ
+{
+    public sealed class Producer
+    {
+        //====================
+        // 
+        //====================
+        private IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private ISession _session;
+
+        private readonly string _topic;
+
+        public Producer(string uri, string topic)
+        {
+            try
+            {
+                _topic = string.Concat(topic, "?consumer.dispatchAsync=false&consumer.prefetchSize=1&consumer.retroactive=true");
+
+                _connectionFactory = new ConnectionFactory(uri);
+                _connection = _connectionFactory.CreateConnection();
+                _connection.Start();
+
+                _session = _connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                topic = null;
+                uri = null;
+            }
+        }
+
+        public void SendMessageMap(Dictionary<string, string> keyValuePairs)
+        {
+            IDestination destination;
+            IMapMessage mapMessage;
+
+            try
+            {
+                destination = _session.GetTopic(_topic);
+
+                //destination = _session.GetDestination(DESTINATION);
+
+                using (IMessageProducer producer = _session.CreateProducer(destination))
+                {
+                    mapMessage = producer.CreateMapMessage();
+
+                    foreach (KeyValuePair<string, string> keyValuePair in keyValuePairs)
+                    {
+                        mapMessage.Body.SetString(keyValuePair.Key, keyValuePair.Value);
+                    }
+
+                    producer.Send(mapMessage);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                destination = null;
+                mapMessage = null;
+            }
+        }
+    }
+}
